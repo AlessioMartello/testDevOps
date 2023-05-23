0d73ef086ffd801d26cef55438ba27fa7ad08c45
AlessioMartello <45970352+AlessioMartello@users.noreply.github.com>
2023-05-23 19:51:43 +0100

    Update triggerDefinitionUAT.ps1

--- itv-development-data-factory/triggerDefinitionUAT.ps1
+++ itv-development-data-factory/triggerDefinitionUAT.ps1
@@ -3,43 +3,43 @@ param (
     $testArgPS
 )
 
-$apimServiceName = $testArgPS
+$triggerStartTime =[int]$testArgPS
 
-Write-Host "Api Management Service Name: $($apimServiceName)"
+Write-Host "Api Management Service Name: $($triggerStartTime)"
 
-# $ def = {
-#     "name": "trigger1",
-#     "properties": {
-#         "annotations": [],
-#         "runtimeState": "Started",
-#         "pipelines": [
-#             {
-#                 "pipelineReference": {
-#                     "referenceName": "test",
-#                     "type": "PipelineReference"
-#                 }
-#             }
-#         ],
-#         "type": "ScheduleTrigger",
-#         "typeProperties": {
-#             "recurrence": {
-#                 "frequency": "Day",
-#                 "interval": 1,
-#                 "startTime": "2023-05-23T16:37:00Z",
-#                 "timeZone": "UTC",
-#                 "schedule": {
-#                     "hours": [
-#                         10
-#                     ]
-#                 }
-#             }
-#         }
-#     }
-# }
+$ def = {
+    "name": "trigger1",
+    "properties": {
+        "annotations": [],
+        "runtimeState": "Started",
+        "pipelines": [
+            {
+                "pipelineReference": {
+                    "referenceName": "test",
+                    "type": "PipelineReference"
+                }
+            }
+        ],
+        "type": "ScheduleTrigger",
+        "typeProperties": {
+            "recurrence": {
+                "frequency": "Day",
+                "interval": 1,
+                "startTime": "2023-05-23T16:37:00Z",
+                "timeZone": "UTC",
+                "schedule": {
+                    "hours": [
+                        $triggerStartTime
+                    ]
+                }
+            }
+        }
+    }
+}
 
 
-# Stop-AzDataFactoryV2Trigger -ResourceGroupName "rg-devops-deploy-dev" -DataFactoryName "Target-ADF" -TriggerName "trigger1" -Force
+Stop-AzDataFactoryV2Trigger -ResourceGroupName "rg-devops-deploy-dev" -DataFactoryName "Target-ADF" -TriggerName "trigger1" -Force
 
-# Set-AzDataFactoryV2Trigger -ResourceGroupName "rg-devops-deploy-dev" -DataFactoryName "Target-ADF" -Name "trigger1" -DefinitionFile "$(System.DefaultWorkingDirectory)/_amartello-CI/drop/triggerDefinitionUAT.json" -Force
+Set-AzDataFactoryV2Trigger -ResourceGroupName "rg-devops-deploy-dev" -DataFactoryName "Target-ADF" -Name "trigger1" -DefinitionFile "$(System.DefaultWorkingDirectory)/_amartello-CI/drop/triggerDefinitionUAT.json" -Force
 
-# Start-AzDataFactoryV2Trigger -ResourceGroupName "rg-devops-deploy-dev" -DataFactoryName "Target-ADF" -TriggerName "trigger1" -Force
+Start-AzDataFactoryV2Trigger -ResourceGroupName "rg-devops-deploy-dev" -DataFactoryName "Target-ADF" -TriggerName "trigger1" -Force

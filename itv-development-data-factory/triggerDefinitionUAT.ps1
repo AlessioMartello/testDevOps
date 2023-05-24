ce4a9db78b9ad7e6b6d2b3db316ec92c2eb42842
AlessioMartello <45970352+AlessioMartello@users.noreply.github.com>
2023-05-24 09:12:55 +0100

    Update triggerDefinitionUAT.ps1

--- itv-development-data-factory/triggerDefinitionUAT.ps1
+++ itv-development-data-factory/triggerDefinitionUAT.ps1
@@ -1,47 +1,14 @@
-[CmdletBinding()]
-param (
-    $testArgPS
-)
+# [CmdletBinding()]
+# param (
+#     $testArgPS
+# )
 
-$triggerStartTime =[int]$testArgPS
+# $triggerStartTime =[int]$testArgPS
 
-Write-Host "Api Management Service Name: $($triggerStartTime)"
-
-$def = @"
-{
-    "name": "trigger1",
-    "properties": {
-        "annotations": [],
-        "runtimeState": "Started",
-        "pipelines": [
-            {
-                "pipelineReference": {
-                    "referenceName": "test",
-                    "type": "PipelineReference"
-                }
-            }
-        ],
-        "type": "ScheduleTrigger",
-        "typeProperties": {
-            "recurrence": {
-                "frequency": "Day",
-                "interval": 1,
-                "startTime": "2023-05-23T16:37:00Z",
-                "timeZone": "UTC",
-                "schedule": {
-                    "hours": [
-                        12
-                    ]
-                }
-            }
-        }
-    }
-}
-"@
-
+# Write-Host "Api Management Service Name: $($triggerStartTime)"
 
 Stop-AzDataFactoryV2Trigger -ResourceGroupName "rg-devops-deploy-dev" -DataFactoryName "Target-ADF" -TriggerName "trigger1" -Force
 
-Set-AzDataFactoryV2Trigger -ResourceGroupName "rg-devops-deploy-dev" -DataFactoryName "Target-ADF" -Name "trigger1" -DefinitionFile $def -Force
+Set-AzDataFactoryV2Trigger -ResourceGroupName "rg-devops-deploy-dev" -DataFactoryName "Target-ADF" -Name "trigger1" -DefinitionFile "$(System.DefaultWorkingDirectory)/_amartello-CI/drop/DefinitionFileUAT.ps1" -Force
 
 Start-AzDataFactoryV2Trigger -ResourceGroupName "rg-devops-deploy-dev" -DataFactoryName "Target-ADF" -TriggerName "trigger1" -Force
